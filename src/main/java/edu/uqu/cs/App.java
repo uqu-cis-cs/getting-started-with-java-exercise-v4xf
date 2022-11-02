72419fc83a93e24b1b4b993d87607bb19a3f5190
v4xf <112865345+v4xf@users.noreply.github.com>
2022-11-02 23:24:14 +0300

    Faris

--- src/main/java/edu/uqu/cs/App.java
+++ src/main/java/edu/uqu/cs/App.java
@@ -6,15 +6,14 @@ package edu.uqu.cs;
  * 1. Rename the class name from FirstExercise to App
  * 2. Add the public keyword before the class keyword
  * 3. Fix the main mehtod declaration
- * 4. Before submiting your code, modify the text “Hello World” to “Hello: <Name>, <Student ID>” in App.java
- *      a.	Change <Name> to your name
- *      b.	Change <Student ID> to your student ID
+ * 4. Before submiting your code, modify the text “Hello World” to “Hello:
+ * <Name>, <Student ID>” in App.java
+ * a. Change <Name> to your name
+ * b. Change <Student ID> to your student ID
  */
- 
-public class app
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello s443009013" );
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Faris 443009013");
     }
 }
